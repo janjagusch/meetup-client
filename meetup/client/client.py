05cca815639d2513fd93a343f1be0fe2a043be36
jjagusch <jan@instaffo.de>
2020-08-31 16:19:17 +0200

    fix scan for single page requests and fix progress bar

--- meetup/client/client.py
+++ meetup/client/client.py
@@ -79,7 +79,7 @@ class Client:
         Returns:
             int: The total count of retrievable elements.
         """
-        return int(response.headers["X-Total-Count"])
+        return int(response.headers.get("X-Total-Count", 0))
 
     def _scan(self, *, url, params=None, sleep_time=0.1):
         """
@@ -103,7 +103,7 @@ class Client:
             yield res
             total_count = self._get_total_count(res)
             total_requests = math.ceil(total_count / params["page"])
-        for offset in tqdm(range(params["offset"] + 1, total_requests)):
+        for offset in tqdm(range(params["offset"] + 1, total_requests), initial=1, total=total_requests):
             params["offset"] = offset
             sleep(sleep_time)
             res = self._get(url=url, params=params)
